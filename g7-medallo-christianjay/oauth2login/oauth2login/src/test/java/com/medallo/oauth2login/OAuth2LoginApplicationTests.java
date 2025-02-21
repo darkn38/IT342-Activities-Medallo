5b362a8acfae9f73c3500895e3d00896e75edc80
LAB14_USER ON WS38 <L14X20W38@citulab.net>
2025-02-21 18:37:50 +0800

    Added g7-medallo-christianjay folder

---
+++ g7-medallo-christianjay/oauth2login/oauth2login/src/test/java/com/medallo/oauth2login/OAuth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medallo.oauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
